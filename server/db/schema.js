a5520c0d7a57926a3a83b88c61c4132d3303a074
rishabhguptajs <rishabhgupta4523@gmail.com>
2025-02-03 15:58:11 +0530

    Add users table schema to database definition

--- server/db/schema.js
+++ server/db/schema.js
@@ -1,5 +1,13 @@
 import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
+export const usersTable = pgTable('users', {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    email: text().notNull(),
+    password: text().notNull(),
+    createdAt: timestamp('created_at').defaultNow(),
+    updatedAt: timestamp('updated_at').$onUpdate(() => new Date())
+})
+
 export const todosTable = pgTable('todos', {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     todo: text().notNull(),
