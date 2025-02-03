0cf1f87797e120be1f4088610c4a0225fd3eb984
rishabhguptajs <rishabhgupta4523@gmail.com>
2025-02-03 15:03:05 +0530

    --init dashboard

---
+++ client/src/app/chat/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <div>
+      Here the main chatting part, as in the interaction with the system in natural language for adding a new task, checking the status of the tasks, etc. will be implemented, in a sleek and user-friendly UI.
+    </div>
+  )
+}
+
+export default Chat
